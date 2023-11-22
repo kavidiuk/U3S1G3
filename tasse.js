c38524f8a4ef319cc51641a5ec124eb264b9e7e8
kavidu <kaviduwarnakulasuriya@gmail.com>
2023-11-22 17:04:54 +0100

    U3S1G3

---
+++ tasse.js
@@ -0,0 +1,8 @@
+var Tasse = /** @class */ (function () {
+    function Tasse(_utileTassa, _tasseIrpef, _tasseInps) {
+        this.utileTassa = _utileTassa;
+        this.tasseIrpef = _tasseIrpef;
+        this.tasseInps = _tasseInps;
+    }
+    return Tasse;
+}());
---
+++ tasse.ts
@@ -0,0 +1,11 @@
+class Tasse{
+    utileTassa:number;
+    tasseIrpef:number;
+    tasseInps:number;
+    constructor (_utileTassa:number , _tasseIrpef:number , _tasseInps:number){
+        this.utileTassa=_utileTassa;
+        this.tasseIrpef=_tasseIrpef;
+        this.tasseInps=_tasseInps;
+    }
+    
+}
