363126f4eff114169040802198179cce38ec84f3
kavidu <kaviduwarnakulasuriya@gmail.com>
2023-11-23 01:11:41 +0100

    first commit

--- tasse.js
+++ tasse.js
@@ -1,8 +1,27 @@
 var Tasse = /** @class */ (function () {
-    function Tasse(_utileTassa, _tasseIrpef, _tasseInps) {
-        this.utileTassa = _utileTassa;
-        this.tasseIrpef = _tasseIrpef;
-        this.tasseInps = _tasseInps;
+    function Tasse(_redditoAnnuoLordo, _inps, _irpef) {
+        this.redditoAnnuoLordo = _redditoAnnuoLordo;
+        this.inps = _inps / 100;
+        this.irpef = _irpef / 100;
     }
+    Tasse.prototype.calcoloTasse = function () {
+        var tasseIrpef = this.redditoAnnuoLordo * this.irpef;
+        var tasseInps = this.redditoAnnuoLordo * this.inps;
+        var getRedditoAnnuoNetto = this.redditoAnnuoLordo - tasseIrpef - tasseInps;
+        return {
+            tasseIrpef: tasseIrpef,
+            tasseInps: tasseInps,
+            getRedditoAnnuoNetto: getRedditoAnnuoNetto
+        };
+    };
     return Tasse;
 }());
+var professionista = new Tasse(100000, 25, 5);
+var risultatoProfessionista = professionista.calcoloTasse();
+console.table(risultatoProfessionista);
+var artigiano = new Tasse(100000, 15, 15);
+var risultatoArtigiano = artigiano.calcoloTasse();
+console.table(risultatoArtigiano);
+var commerciante = new Tasse(100000, 15, 35);
+var risultatoCommerciante = commerciante.calcoloTasse();
+console.table(risultatoCommerciante);
--- tasse.ts
+++ tasse.ts
@@ -1,11 +1,36 @@
-class Tasse{
-    utileTassa:number;
-    tasseIrpef:number;
-    tasseInps:number;
-    constructor (_utileTassa:number , _tasseIrpef:number , _tasseInps:number){
-        this.utileTassa=_utileTassa;
-        this.tasseIrpef=_tasseIrpef;
-        this.tasseInps=_tasseInps;
-    }
-    
+class Tasse {
+  inps: number;
+  irpef: number;
+  redditoAnnuoLordo: number;
+  constructor(_redditoAnnuoLordo: number, _inps: number, _irpef: number) {
+    this.redditoAnnuoLordo = _redditoAnnuoLordo;
+    this.inps = _inps/100;
+    this.irpef = _irpef/100;
+  }
+
+  calcoloTasse() {
+    const tasseIrpef = this.redditoAnnuoLordo * this.irpef;
+    const tasseInps = this.redditoAnnuoLordo * this.inps;
+    const getRedditoAnnuoNetto =
+      this.redditoAnnuoLordo - tasseIrpef - tasseInps;
+
+    return {
+      tasseIrpef,
+      tasseInps,
+      getRedditoAnnuoNetto
+    };
+  }
 }
+
+let professionista = new Tasse(100000, 25, 5);
+let risultatoProfessionista = professionista.calcoloTasse();
+console.table(risultatoProfessionista);
+
+
+let artigiano = new Tasse (100000,15,15);
+let risultatoArtigiano = artigiano.calcoloTasse();
+console.table(risultatoArtigiano);
+
+let commerciante = new Tasse(100000,15,35);
+let risultatoCommerciante = commerciante.calcoloTasse();
+console.table(risultatoCommerciante);
